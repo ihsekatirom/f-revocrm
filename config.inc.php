0fe6638993912dcdef46e2005dcba73b2695540d
irom <irom.reg2015@gmail.com>
2018-08-15 23:24:07 +0900

    site_URL変更

--- config.inc.php
+++ config.inc.php
@@ -82,7 +82,7 @@ $dbconfigoption['ssl'] = false;
 
 $host_name = $dbconfig['db_hostname'];
 
-$site_URL =  'http://54.210.135.241/f-revocrm/';
+$site_URL = 'http://34.234.193.0/f-revocrm/';
 
 // url for customer portal (Example: http://vtiger.com/portal)
 $PORTAL_URL = $site_URL.'/customerportal';
