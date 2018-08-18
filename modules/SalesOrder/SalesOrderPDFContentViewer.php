2a46d7a5ea10736a2d0c47b46766b4d346b56c0d
irom <irom.reg2015@gmail.com>
2018-08-18 18:39:02 +0900

    debug

--- modules/SalesOrder/SalesOrderPDFContentViewer.php
+++ modules/SalesOrder/SalesOrderPDFContentViewer.php
@@ -50,7 +50,8 @@ class SalesOrderPDFContentViewer extends Vtiger_PDF_InventoryContentViewer {
 					$pdf->MultiCell($cellWidth, $this->headerRowHeight, $cellLabel, 1, 'L', 1, 1, $contentFrame->x+$offsetX, $contentFrame->y);
 					$pdf->SetFont('','');
 					$pdf->SetFontSize(6);
-					$pdf->MultiCell($cellWidth, $this->headerRowHeight/2, $descript, 0, 'L', 0, 0, $contentFrame->x+$offsetX, $contentFrame->y+$this->headerRowHeight/2);
+					$pdf->MultiCell($cellWidth-35, $this->headerRowHeight, $descript, 0, 'L', 0, 0, $contentFrame->x+$offsetX, $contentFrame->y);
+//					$pdf->MultiCell($cellWidth, $this->headerRowHeight/2, $descript, 0, 'L', 0, 0, $contentFrame->x+$offsetX, $contentFrame->y+$this->headerRowHeight/2);
 					$pdf->SetFont('','B');
 					$pdf->SetFontSize(12);
 				}
