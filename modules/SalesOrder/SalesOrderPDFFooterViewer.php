0b43608557c4c1eaa5bdd371141be0cd21a97dec
irom <irom.reg2015@gmail.com>
2018-08-18 20:24:35 +0900

    debug

--- modules/SalesOrder/SalesOrderPDFFooterViewer.php
+++ modules/SalesOrder/SalesOrderPDFFooterViewer.php
@@ -45,17 +45,17 @@ class SalesOrderPDFFooterViewer extends Vtiger_PDF_InventoryFooterViewer {
 		if($this->model) {
 			$targetFooterHeight = ($this->onEveryPage())? $footerFrame->h : 0;
 
-			/***
 			$descriptionString = $this->labelModel->get(self::$DESCRIPTION_LABEL_KEY);
 			$description = $this->model->get(self::$DESCRIPTION_DATA_KEY);
 			$descriptionWidth = $pdf->GetStringWidth($descriptionString, $footerFrame->w);
 			$pdf->SetFillColor(205,201,201);
-			$pdf->SetFont('', 'B');
+//			$pdf->SetFont('', 'B');
 			$pdf->MultiCell($descriptionWidth, $descriptionHeight, $descriptionString, 1, 'L', 1, 1, $footerFrame->x, $footerFrame->y);
-			$pdf->SetFont('', '');
-			$pdf->MultiCell($footerFrame->w - $descriptionWidth, $targetFooterHeight - $descriptionHeight, $description, 1, 'L',
-				0, 1, $footerFrame->x + $descriptionWidth, $footerFrame->y);
-				***/
+//			$pdf->SetFont('', '');
+			$pdf->MultiCell($footerFrame->w - $descriptionWidth, $targetFooterHeight - $descriptionHeight, $description, 1, 'L', 0, 1,
+				$footerFrame->x + $descriptionWidth, $footerFrame->y);
+
+/***
 			$termsAndConditionLabelString = $this->labelModel->get(self::$TERMSANDCONDITION_LABEL_KEY);
 			$termsAndCondition = $this->model->get(self::$TERMSANDCONDITION_DATA_KEY);
 			$offsetY = 2.0;
@@ -65,7 +65,7 @@ class SalesOrderPDFFooterViewer extends Vtiger_PDF_InventoryFooterViewer {
 //				$footerFrame->x, $footerFrame->y);
 			$pdf->MultiCell($footerFrame->w, $targetFooterHeight - $termsAndConditionHeight, $termsAndCondition,1, 'L', 0, 1,
 		 	 $footerFrame->x, $footerFrame->y);
-
+***/
 		}
 	}
 }
