0f79c2fbbb288b83d49761bee59a2c361ad6d7bb
irom <irom.reg2015@gmail.com>
2018-08-18 17:18:10 +0900

    debug

--- modules/SalesOrder/SalesOrderPDFController.php
+++ modules/SalesOrder/SalesOrderPDFController.php
@@ -365,7 +365,7 @@ class Vtiger_SalesOrderPDFController extends Vtiger_InventoryPDFController{
 					" LEFT JOIN vtiger_users" .
 					" ON vtiger_users.id = vtiger_crmentity.smownerid" .
 					" WHERE vtiger_crmentity.deleted = 0 and accountid = ?";
-			$params = array($this->focusColumnValue('parent_id'));
+			$params = array($this->focusColumnValue('account_id'));
 			$result = $adb->pquery($query, array());
 			$num_rows = $adb->num_rows($res);
 
@@ -381,7 +381,7 @@ class Vtiger_SalesOrderPDFController extends Vtiger_InventoryPDFController{
 			$modelColumn2 = array(
 				'print_date' => decode_html($this->joinValues($printInfo)),
 				'summary' => decode_html($resultrow['organizationname']),
-				'content' => decode_html($this->joinValues($addressValues, ' '). $this->joinValues($additionalCompanyInfo, ' '). $this->joinValues($userValues, ' ').$query)
+				'content' => decode_html($this->joinValues($addressValues, ' '). $this->joinValues($additionalCompanyInfo, ' '). $this->joinValues($userValues, ' ').)
 				);
 			}
 		return $modelColumn2;
